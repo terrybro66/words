fee7ac1c1621c5435d3e83d1e919d00b6993a55c
terrybro66 <terrybro61@gmail.com>
2023-04-03 17:37:01 +0100

    adding random words

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./Header.module.css"; // import the CSS module
 
 const Header = () => {
+  const [word, setWord] = useState("");
+
+  const words = ["cat", "dog", "bird", "fish", "mouse"];
   return (
     <div className={styles.container}>
-      <div></div>
-      <div className={styles.letter}>
-        <input type="text"></input>
-      </div>
-      <div className={styles.letter}>
-        <input type="text"></input>
-      </div>
-      <div className={styles.letter}>
-        <input type="text"></input>
+      <div>
+        {words[1].split("").map((letter) => {
+          return (
+            <div className={styles.letter}>
+              <input
+                type="text"
+                placeholder={letter}
+                onChange={handleChange}
+              ></input>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
