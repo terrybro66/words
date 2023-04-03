1624742841b9d72e21ef4b693167e5845d94b59b
terrybro66 <terrybro61@gmail.com>
2023-04-03 13:28:38 +0100

    adding container into main

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,9 @@
-export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
-}
+import React from "react";
+
+import styles from "./Header.module.css"; // import the CSS module
+
+const Header = () => {
+  return <div className={styles.container}></div>;
+};
+
+export default Header;
