40e9a6a140e2093ee8e4fb7953b3f57723e526bc
terrybro66 <terrybro61@gmail.com>
2023-04-03 13:57:04 +0100

    adding width

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,11 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Words!</title>
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
       </main>
     </div>
   );
